38b36049ffd5cf8c5f1ea4ff5691f3c868498341
Guchito <a_gaggero@hotmail.com>
2024-02-05 23:39:06 +0100

    Basic layout

---
+++ src/components/Form.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from '@emotion/styled'
+
+
+const InputSubmit = styled.input
+
+const Form = () => {
+  return (
+    <form>
+
+        <InputSubmit type='submit' value='Quote' />
+
+    </form>
+  )
+}
+
+export default Form
